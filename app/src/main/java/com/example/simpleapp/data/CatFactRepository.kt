ce37449560b92dd034ee48dee7123500e487aa3d
Daniel Molinero Reguera <dmolinero@groupon.com>
2020-04-01 09:57:43 -0700

    Adding comment for improvement on Repository

--- app/src/main/java/com/example/simpleapp/data/CatFactRepository.kt
+++ app/src/main/java/com/example/simpleapp/data/CatFactRepository.kt
@@ -5,6 +5,10 @@ import com.example.simpleapp.data.network.RetrofitProvider
 import com.example.simpleapp.model.CatFact
 import io.reactivex.Single
 
+// It would be better to wrap the return value as a Data class,
+// that way we don't expose the network errors to the outside,
+// as now the VM is dealing with the network errors.
+// ENCAPSULATION AND SEPARATION OF CONCERNS!
 class CatFactRepository {
     fun getCatFacts(from: Int, length: Int): Single<List<CatFact>> {
         val apiClient = RetrofitProvider.get().create(CatFactApiClient::class.java)
